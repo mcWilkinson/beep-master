80a0b06068eb0e27cf0754c945083b5857178ced
Connor W <mwilkinson@ggc.edu>
2017-10-04 11:56:07 +0000

    Added notes.factory.js contents

--- app/components/notes/notes.factory.js
+++ app/components/notes/notes.factory.js
@@ -1,5 +1,14 @@
 (function() {
     'use strict';
     
+    angular.module('beep-master')
     
+    .factory('NotesFactory', NotesFactory);
+    
+    function NotesFactory() {
+        var vm = this;
+        vm.pitches = [{name: 'C#', pitch: 400},{name: 'D', pitch: 500}];
+        
+        return vm;
+    }
 })();
